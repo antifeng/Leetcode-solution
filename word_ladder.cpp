d83ecbff313c9f72d4f59110d4c835c1c8d88f5e
antifeng <ylfeng1986@gmail.com>
2013-07-02 11:38:43 -0400

    Create word_ladder.cpp

---
+++ word_ladder.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    /**
+      *The key idea is to apply BFS to enumerate all morph words from inner core to outer ring
+      *To erase valid morph word from dict is also important to reduct running time
+    */
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        queue<string> CurRingVec;
+        queue<string> NextRingVec;
+        int idist = 1;
+      unordered_set<string>::iterator itorend = dict.end();
+    
+    	CurRingVec.push(start);
+        
+    	while(!CurRingVec.empty()) {
+    		string strSrch = CurRingVec.front();
+    		int len = strSrch.length();
+    		string strmorph;
+    		for (int i=0; i<len; i++) {
+                strmorph = strSrch;
+    			for (char ch='a'; ch <= 'z'; ch++) {
+    				strmorph[i] = ch;
+    				if ( strmorph==end )
+    					return idist+1;
+    				if (strmorph !=strSrch && dict.find(strmorph)!=itorend){
+    					NextRingVec.push(strmorph);
+                        dict.erase(strmorph);
+    				}
+    			}
+    		}
+    		
+    		CurRingVec.pop();
+    		if (CurRingVec.empty() && (!NextRingVec.empty())) {
+    			idist++;
+                queue<string> tmp;
+                tmp = CurRingVec;
+                CurRingVec = NextRingVec;
+                NextRingVec = tmp;
+    		}
+    	}//out of while
+        return 0;
+    }
+};
