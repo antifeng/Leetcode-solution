978b01bcd458abee15dcec8c4dacc192e0212843
antifeng <ylfeng1986@gmail.com>
2013-09-04 16:19:34 -0400

    Create StockTrading.cpp
    Solution of Best Time to Buy and Sell Stock

---
+++ StockTrading.cpp
@@ -0,0 +1,40 @@
+
+class Solution {
+public:
+    //idea is to find the index of max and min element
+    //Then divide the array into three parts.
+ 	//Divide and conque
+int getmaxProfit(vector<int> &prices, int s, int t) {
+    
+    if (s >= t)
+		return 0;
+    int minIdx, maxIdx;
+	minIdx = s;
+	maxIdx = t;
+    
+	for(int i=s; i<=t; i++) {
+		if (prices[minIdx]> prices[i])
+			minIdx = i;
+		else if (prices[maxIdx] < prices[i])
+			maxIdx = i;
+	}
+	if (minIdx < maxIdx) 
+		return prices[maxIdx] - prices[minIdx];
+	else {
+        int left = getmaxProfit(prices, s, maxIdx);
+        int right = getmaxProfit(prices, minIdx, t);
+        int mid = getmaxProfit(prices, maxIdx+1, minIdx-1); 
+        
+        return left > right ? ( mid > left ?  mid : left) : ( mid > right ?  mid : right);
+	}
+}
+
+int maxProfit(vector<int> &prices) {
+    
+    int size = prices.size();
+	if (prices.empty() || size==1)
+		return 0;
+	
+	return getmaxProfit(prices, 0, size-1);
+    }
+};
