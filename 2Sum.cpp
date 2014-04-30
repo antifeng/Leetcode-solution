924a809651f2ee4803fc6b98d941a31ac4edc075
antifeng <ylfeng1986@gmail.com>
2014-04-29 22:12:05 -0400

    Update 2Sum.cpp

--- 2Sum.cpp
+++ 2Sum.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    //My first attemp is to sort the number, then I found the return value is the original index which dismiss my prev thought
-    //Note: do not initilize the vector with specific size since if so the vector will be filled with n element unless you know how much ele will be set
+    //Note: the input array is orderless, to improve 
+    //the performance, we need to sort it first
+    //then get two number whose sum equal to the target
+    //finally get the index from the original array
+    //The whole complexity is nlog(n)
     vector<int> twoSum(vector<int> &numbers, int target) {
     	vector<int> res(2);
     	if (numbers.empty() || numbers.size() < 2)
 			return res;
-		int len = numbers.size();
-
-		for (int j=0; j<len-1; j++)
-		{
-			for (int k=len-1; k>j; k--) {
-				if (numbers[j]+numbers[k] == target){
-					res[0] = j+1;
-                    res[1] = k+1;
-					return res;
-				}
-			}
+		
+        vector<int> TmpVec(numbers);
+        sort(TmpVec.begin(), TmpVec.end());
+        
+	for (int j=0, k=numbers.size()-1; j<k;) {
+		int nSum = TmpVec[j]+TmpVec[k];
+		if ( nSum < target)
+			j++;
+		else if ( nSum > target)
+			k--;
+		else {
+		//get the original index of those numbers
+			res[0] = TmpVec[j];
+			res[1] = TmpVec[k];
+			auto first = find_first_of(numbers.begin(), numbers.end(), res.begin(), res.end());
+			auto second = find_first_of(first+1, numbers.end(), res.begin(), res.end());
+			res[0] = 1 + first-numbers.begin();
+			res[1] = 1 + second-numbers.begin();
+			return res;
 		}
+	} //end for
         return res;
-	}
+   }
 };
