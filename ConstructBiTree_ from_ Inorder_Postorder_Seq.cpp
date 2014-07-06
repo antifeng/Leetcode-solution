e57f1e00ea20dc0f5f9a90e10ce2c5b48506bff8
antifeng <ylfeng1986@gmail.com>
2014-07-06 16:36:54 -0400

    Update ConstructBiTree_ from_ Inorder_Postorder_Seq.cpp

--- ConstructBiTree_ from_ Inorder_Postorder_Seq.cpp
+++ ConstructBiTree_ from_ Inorder_Postorder_Seq.cpp
@@ -8,11 +8,10 @@
  * };
  */
 class Solution {
-
 private:
 	//Key idea: the last element of postorder seq is the root node which can be used to divide inorder seq
-     typedef vector<int>::iterator iter;
-    TreeNode *buildTreeRecursive(iter instart, iter inend, iter poststart, iter postend) {
+    typedef vector<int>::iterator iter;
+    TreeNode *buildTreeHelper(iter instart, iter inend, iter poststart, iter postend) {
 		TreeNode* ptree = NULL;
 		if ( inend == instart || postend == poststart)	
     		return ptree;
@@ -26,18 +25,18 @@ private:
 			ptree = new TreeNode(*(postend-1));
 			for (iter miditor = instart; miditor < inend; miditor++) {
 				if (*miditor == *(postend-1)) { //Calc the position of iterator is not easy
-						ptree->left = buildTreeRecursive(instart, miditor, poststart, poststart+(miditor-instart));
-                		ptree->right = buildTreeRecursive(miditor+1, inend, postend-(inend-miditor), postend-1);
+						ptree->left = buildTreeHelper(instart, miditor, poststart, poststart+(miditor-instart));
+                		ptree->right = buildTreeHelper(miditor+1, inend, postend-(inend-miditor), postend-1);
 				}
 			}//end for
 		}//end else
         return ptree;
 	}
+	
 public:
-
     TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
 		TreeNode* ptree = NULL;		
-		ptree = buildTreeRecursive(inorder.begin(), inorder.end(), postorder.begin(), postorder.end());
+		ptree = buildTreeHelper(inorder.begin(), inorder.end(), postorder.begin(), postorder.end());
 		return ptree;
 	}
 };
